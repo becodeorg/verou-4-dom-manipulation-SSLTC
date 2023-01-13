97b4808280cb48778f7f210ebeaa6458fb1b9f38
Snoeck Sven <snoeck.sven@live.be>
2023-01-13 15:58:23 +0100

    The page is made responsive, a heading is added and a search field which is not working yet
    Made it responsive with mediaqueries, a nav section with a search field and a wite on black heading

--- Collection-Project/script.js
+++ Collection-Project/script.js
@@ -1,9 +1,19 @@
 import { collectionBooks } from "./collection.js";
 
-const elHead = document.querySelector("head");
+const elHead = document.querySelector("header");
 const elHeading1 = document.createElement("h1");
-elHeading1.textContent = "Heading";
+elHeading1.textContent = "My book collection";
 elHead.appendChild(elHeading1);
+
+const elNav = document.querySelector("nav");
+const elInputSearchOnTitle = document.createElement("input");
+elInputSearchOnTitle.setAttribute("id", "inputSearchOnTitle");
+
+const elLabelSearchOnTitle = document.createElement("label");
+elLabelSearchOnTitle.setAttribute("for", "inputSearchOnTitle");
+elLabelSearchOnTitle.setAttribute("id", "lblSearchOnTitle");
+elLabelSearchOnTitle.textContent = "Search on title";
+elNav.append(elInputSearchOnTitle, elLabelSearchOnTitle);
 
 const elGrid = document.querySelector("main");
 
@@ -54,7 +64,10 @@ collectionBooks.forEach(item => {
     elImageBookstore.setAttribute("id", "iconBookstore");
     elImageBookstore.setAttribute("src", "./img/icon-standaardboekhandel.png");
     elImageBookstore.setAttribute("alt", "Standaard boekhandel icon, link to book");
+    elImageBookstore.setAttribute("title", "Link to the bookstore");
     elImageBookstore.addEventListener('click', () => { window.location = item.Link});
+    // Change cursor to pointer when hover over link
+    elImageBookstore.addEventListener("mouseover", (e) => { elImageBookstore.style.cursor = "pointer" });
 
     elItemBody.append(
         elSpanReleaseDate,
@@ -69,4 +82,3 @@ collectionBooks.forEach(item => {
 
     elGrid.appendChild(elGridItem);
 });
-
